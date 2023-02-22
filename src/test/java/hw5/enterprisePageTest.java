12453a631f12b017ad9679d4a69edcf8c56e7425
Lissa Vasenko <lisa.vasenko@outlook.com>
2023-02-22 21:52:45 +0300

    enterprisePageTest.java was developed

---
+++ src/test/java/hw5/enterprisePageTest.java
@@ -0,0 +1,18 @@
+package hw5;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class enterprisePageTest {
+    @Test
+    void enterprisePageChecks(){
+        open("https://github.com");
+        $$(".HeaderMenu-link").findBy(text("Solutions")).hover();
+        $$(".HeaderMenu-dropdown-link").findBy(text("Enterprise")).click();
+        $(byText("Build like the best")).shouldBe(visible);
+    }
+}
