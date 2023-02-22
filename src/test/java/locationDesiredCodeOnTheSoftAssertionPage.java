def9a88d1f3aafeb251805a4496b97f640f19ba4
Lissa Vasenko <lisa.vasenko@outlook.com>
2023-02-22 20:53:48 +0300

    unnecessary import has been removed

--- src/test/java/locationDesiredCodeOnTheSoftAssertionPage.java
+++ src/test/java/locationDesiredCodeOnTheSoftAssertionPage.java
@@ -2,7 +2,6 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.AssertionMode.SOFT;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
